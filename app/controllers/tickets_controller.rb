582ccf67b18fa8a892d9b0035c7a50f12d7ff3e5
Marcin Słowik <riobson@gmail.com>
2015-05-20 14:52:17 +0000

    add show/edit/destroy tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,38 @@
 class TicketsController < ApplicationController
+  def index
+    @tickets = Ticket.all
+  end
+  def show
+    @ticket = Ticket.find(params[:id])
+  end
   def new
   end
   def create
-    render plain: params[:tickets].inspect
-end
+   @ticket = Ticket.new(ticket_params)
+ 
+   if @ticket.save
+     redirect_to @ticket
+    else
+      render 'new'
+    end
+  end
+  def update
+    @ticket = Ticket.find(params[:id])
+ 
+    if @ticket.update(ticket_params)
+      redirect_to @ticket
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @ticket = Ticket.find(params[:id])
+    @ticket.destroy
+ 
+    redirect_to tickets_path
+  end
+  private
+    def ticket_params
+      params.require(:tickets).permit(:title, :text)
+    end
 end
