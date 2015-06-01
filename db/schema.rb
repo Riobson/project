cff90695aa144c6cda3c7b6c29644050b7aeb582
Marcin Słowik <riobson@gmail.com>
2015-06-01 10:26:10 +0000

    Revert "add some fields to header"
    This reverts commit d6995d210477078368fc4740be1aafc569ae5dcd.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519131822) do
+ActiveRecord::Schema.define(version: 20150601102412) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "tickets", force: :cascade do |t|
@@ -25,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150519131822) do
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "project_id"
   end
 
   create_table "users", force: :cascade do |t|
