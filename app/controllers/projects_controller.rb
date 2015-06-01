3a87e3b8f40a85b740678b8c8c06a3d70ed6e9cb
Marcin Słowik <riobson@gmail.com>
2015-06-01 15:11:27 +0000

    change the migration name, and field name in project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -39,6 +39,6 @@ class ProjectsController < ApplicationController
   end
   private
     def project_params
-      params.require(:project).permit(:title, :text)
+      params.require(:project).permit(:title, :description)
     end
 end
--- db/migrate/20150601150050_create_project.rb
+++
@@ -1,6 +0,0 @@
-class CreateProject < ActiveRecord::Migration
-  def change
-    create_table :projects do |t|
-    end
-  end
-end
